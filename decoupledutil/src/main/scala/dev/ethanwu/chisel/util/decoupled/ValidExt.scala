4000c6b47963541a14ecfce50f6b4332e9ca400f
Ethan Wu <ethanwu10@gmail.com>
2023-03-15 16:22:24 -0700

    add `.map` to ValidExt

--- decoupledutil/src/main/scala/dev/ethanwu/chisel/util/decoupled/ValidExt.scala
+++ decoupledutil/src/main/scala/dev/ethanwu/chisel/util/decoupled/ValidExt.scala
@@ -28,6 +28,21 @@ object ValidExt {
       target.valid := false.B
       target.bits := DontCare
     }
-  }
 
+    /** Applies the supplied functor to the bits of this interface, returning a
+      * new typed [[Valid]] interface.
+      *
+      * @param f
+      * The function to apply to this [[Valid]]'s 'bits' with return type B
+      * @return
+      * a new [[Valid]] of type B
+      */
+    def map[B <: Data](f: T => B): Valid[B] = {
+      val _map_bits = f(target.bits)
+      val _map = Wire(Valid(chiselTypeOf(_map_bits)))
+      _map.bits := _map_bits
+      _map.valid := target.valid
+      _map
+    }
+  }
 }
